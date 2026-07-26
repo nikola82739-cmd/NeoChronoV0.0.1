16d3fb7f2f5ba084eebfafc6018e224e813d5497
Lans_ Working <nikola82739@gmail.com>
2026-07-26 17:33:20 +0300

    Update Colors.hpp

--- Colors/Colors.hpp
+++ Colors/Colors.hpp
@@ -1,1 +1,12 @@
+#pragma once
 
+namespace nc_colors {
+    constexpr const char* Reset  = "\033[0m";   // сбросить цвет (обязательно в конце!)
+    constexpr const char* Red    = "\033[31m"; // красный
+    constexpr const char* Green  = "\033[32m"; // зелёный
+    constexpr const char* Yellow = "\033[33m"; // жёлтый
+    constexpr const char* Blue   = "\033[34m"; // синий
+    constexpr const char* Magenta= "\033[35m"; // фиолетовый
+    constexpr const char* Cyan   = "\033[36m"; // голубой
+    constexpr const char* White  = "\033[37m"; // белый
+}
