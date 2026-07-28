d5dbbe79695604216cbcd0ac51a28e9689a15622
Lans_ Working <nikola82739@gmail.com>
2026-07-28 11:13:40 +0300

    Update Colors.hpp

--- Colors/Colors.hpp
+++ Colors/Colors.hpp
@@ -1,1 +1,11 @@
+#pragma once
 
+namespace colors { // Color expansion
+   
+   constexpr const char* RESET  = "\033[0m"; //  Color: "RESET"
+   constexpr const char* RED    = "\033[31m";// Color: "RED"
+   constexpr const char* CYAN   = "\033[36m";// Color: "CYAN"
+   constexpr const char* GREEN  = "\033[32m";// Color: "GREEN"
+   constexpr const char* YELLOW = "\033[33m";// Color: "YELLOW"
+
+}
